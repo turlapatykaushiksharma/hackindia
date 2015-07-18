fb977d9bba1bf35a3e13d1c8a2528f0ea2a62dfc
Manoj Pandey <manojpandey1996@gmail.com>
2015-07-18 15:39:27 +0530

    main

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import os
+from flask import flask
+from flask.ext.bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+
+
+// Classes
+
+
+
+// Routes
+
+
+
+def main ():
+	pass
+
+if __name__ == '__main__':
+	main()
